9bc31fce2312c2606007c62ffec52b53babf9a3a
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-10-18 18:25:25 +0200

    commit 4

--- hatodikFeladat/hatodikFeladat/Form1.cs
+++ hatodikFeladat/hatodikFeladat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using hatodikFeladat.MnbServiceReference;
+﻿using hatodikFeladat.Entities;
+using hatodikFeladat.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,13 @@ namespace hatodikFeladat
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+       
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            dataGridView1.DataSource = Rates;
         }
 
         private void GetExchangeRates()
