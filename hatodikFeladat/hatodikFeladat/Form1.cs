a618f92363987f87e9956f0ff8edc4a6a79214c4
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-10-18 19:35:50 +0200

    commit 6

--- hatodikFeladat/hatodikFeladat/Form1.cs
+++ hatodikFeladat/hatodikFeladat/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace hatodikFeladat
@@ -23,7 +24,28 @@ namespace hatodikFeladat
             InitializeComponent();
             GetExchangeRates();
             XlmFeldolg();
+            AdatMegejel();
             dataGridView1.DataSource = Rates;
+        }
+
+        private void AdatMegejel()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
         }
 
         private void XlmFeldolg()
