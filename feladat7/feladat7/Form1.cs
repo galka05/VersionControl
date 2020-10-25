7ccf07c05bd418cb71c3eb6c8e0f6a7ba503517f
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-10-25 20:41:18 +0100

    commit 4

--- feladat7/feladat7/Form1.cs
+++ feladat7/feladat7/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using feladat7.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,53 @@ namespace feladat7
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
+
+            Person();
+            BirthProbability();
+            DeathProbability();
+
+
+        }
+
+        private void DeathProbability()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BirthProbability()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Person()
+        {
+             List<Person> GetPopulation(string csvpath)
+            {
+                List<Person> population = new List<Person>();
+
+                using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+                        population.Add(new Person()
+                        {
+                            BirthYear = int.Parse(line[0]),
+                            Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                            NbrOfChildren = int.Parse(line[2])
+                        });
+                    }
+                }
+
+                return population;
+            }
         }
     }
 }
