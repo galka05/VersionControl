e205f5cae819676958d953c12475868e4bea68d2
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-11-22 22:13:54 +0100

    commit5

--- kilenc/kilenc/AccountControllerTestFixture.cs
+++ kilenc/kilenc/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace kilenc
 {
@@ -13,6 +14,13 @@ namespace kilenc
         public void TestValidateEmail(string email, bool expectedResult)
         {
             
+            var accountController = new AccountController();
+
+            
+            var actualResult = accountController.ValidateEmail(email);
+
+            
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
